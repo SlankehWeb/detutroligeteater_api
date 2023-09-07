51fa3caa1c02d5474f0069faee4eae868267cbee
Webudvikler Hovedforløb <heka@techcollege.dk>
2023-09-07 14:02:03 +0200

    Reservation By Event ID

--- App/Controllers/reservation.controller.js
+++ App/Controllers/reservation.controller.js
@@ -54,39 +54,6 @@ class ReservationController {
 	}
 
 	/**
-	 * List By Event Metode - henter alle records ud fra event_id
-	 * @param {object} req 
-	 * @param {object} res 
-	 * @return {array} Returnerer JSON array
-	 */
-
-	list_by_event = async (req, res) => {
-		const { id } = req.query
-		const qp = QueryParamsHandle(req, 'id, firstname')
-
-		if(id) {
-			try {
-				const result = await Reservations.findAll({
-					order: [qp.sort_key],
-					limit: qp.limit,
-					attributes: qp.attributes,
-					where: { event_id: id }
-				})
-				// Parser resultat som json
-				res.json(result)				
-			} catch (error) {
-				res.status(418).send({
-					message: `Something went wrong: ${error}`
-				})						
-			}	
-		} else {
-			res.status(403).send({
-				message: 'Wrong parameter values'
-			})
-		}
-	}	
-
-	/**
 	 * GET Metode henter record ud fra id
 	 * @param {object} req 
 	 * @param {object} res 
